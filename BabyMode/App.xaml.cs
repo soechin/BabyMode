1e6a243c1516a1e21dbe5e06dd10d357312467e2
Tzuchin Chang <soechin@gmail.com>
2018-03-24 13:59:08 +0800

    Add project files.

---
+++ BabyMode/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace BabyMode
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
